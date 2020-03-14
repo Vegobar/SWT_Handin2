98f7345896adb6abdf60b08fabc36342b4cac6c0
Mikkel Schmidt Andersen <Mikkel220897@hotmail.com>
2020-03-14 13:40:00 +0100

    Cleaned code

--- SWT_ladeskab/Ladeskab_unit_test/unit_test_stationcontrol.cs
+++ SWT_ladeskab/Ladeskab_unit_test/unit_test_stationcontrol.cs
@@ -1,29 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Castle.Core.Smtp;
-using NSubstitute;
+﻿using NSubstitute;
 using NUnit.Framework;
 using SWT_ladeskab;
-
 
 namespace Ladeskab_unit_test
 {
     public class unit_test_stationcontrol
     {
         [TestFixture]
-        class Sub_tester
+        private class Sub_tester
         {
-
-            private StationControl _stationControl;
-            private IRFIDReader _rfidReader;
-            private IChargeControl _chargeControl;
-            private IDoor _door;
-            private IDisplay _display;
-
-
             [SetUp]
             public void Setup()
             {
@@ -32,21 +17,67 @@ namespace Ladeskab_unit_test
                 _door = Substitute.For<IDoor>();
                 _display = Substitute.For<IDisplay>();
 
-                _stationControl = new SWT_ladeskab.StationControl(_door, _display, _rfidReader,_chargeControl);
+                _stationControl = new StationControl(_door, _display, _rfidReader, _chargeControl);
+            }
+
+            private StationControl _stationControl;
+            private IRFIDReader _rfidReader;
+            private IChargeControl _chargeControl;
+            private IDoor _door;
+            private IDisplay _display;
+
+            [Test]
+            public void test_conncted_phone_false()
+            {
+                _chargeControl.isConnected().Returns(true);
+
+                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs());
+
+                _display.Received(1).display("Forkert RFID tag", 1);
             }
 
             [Test]
-            public void testDoorOpenEventHandler()
+            public void test_connected_phone_true()
             {
-                _door.OpenDoorEvent += Raise.EventWith(new OpenDoorEventArgs());
-                _display.Received(1).display("Tilslut telefon",1);
+                _chargeControl.isConnected().Returns(true);
+
+                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs());
+
+                _display.Received(1).display("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.",
+                    1);
             }
 
             [Test]
-            public void testCloseOpenEventHandler()
+            public void test_LadeSkabsStateLocked_false_id()
             {
-                _door.ClosedDoorEvent += Raise.EventWith(new ClosedDoorEventArgs());
-                _display.Received(1).display("Indlæs RFID",1);
+                _chargeControl.isConnected().Returns(true);
+
+                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs {id = 123});
+                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs {id = 124});
+
+                _display.Received(1).display("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.",
+                    1);
+                _display.Received(1).display("Forkert RFID tag", 1);
+            }
+
+            [Test]
+            public void test_LadeSkabsStateLocked_true_id()
+            {
+                _chargeControl.isConnected().Returns(true);
+
+                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs {id = 123});
+                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs {id = 123});
+
+                _display.Received(1).display("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.",
+                    1);
+                _display.Received(1).display("Tag din telefon ud af skabet og luk døren", 1);
+            }
+
+            [Test]
+            public void testCheckID_false()
+            {
+                var result = _stationControl.CheckId(30, 35);
+                Assert.IsFalse(result);
             }
 
             [Test]
@@ -55,12 +86,19 @@ namespace Ladeskab_unit_test
                 var result = _stationControl.CheckId(30, 30);
                 Assert.IsTrue(result);
             }
-            
+
             [Test]
-            public void testCheckID_false()
+            public void testCloseOpenEventHandler()
             {
-                var result = _stationControl.CheckId(30, 35);
-                Assert.IsFalse(result);
+                _door.ClosedDoorEvent += Raise.EventWith(new ClosedDoorEventArgs());
+                _display.Received(1).display("Indlæs RFID", 1);
+            }
+
+            [Test]
+            public void testDoorOpenEventHandler()
+            {
+                _door.OpenDoorEvent += Raise.EventWith(new OpenDoorEventArgs());
+                _display.Received(1).display("Tilslut telefon", 1);
             }
 
             [Test]
@@ -72,54 +110,6 @@ namespace Ladeskab_unit_test
                 _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs());
                 Assert.True(wasCalled);
             }
-
-            [Test]
-            public void test_connected_phone_true()
-            {
-                _chargeControl.isConnected().Returns(true);
-
-                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs());
-                
-                _display.Received(1).display("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.",1);
-            }
-
-            [Test]
-            public void test_conncted_phone_false()
-            {
-                _chargeControl.isConnected().Returns(true);
-
-                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs());
-                
-                _display.Received(1).display("Forkert RFID tag",1);
-            }
-
-            [Test]
-            public void test_LadeSkabsStateLocked_true_id()
-            {
-                _chargeControl.isConnected().Returns(true);
-
-                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs(){id = 123});
-                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs(){id = 123});
-
-                _display.Received(1).display("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.",1);
-                _display.Received(1).display("Tag din telefon ud af skabet og luk døren",1);
-                
-            }
-            
-            [Test]
-            public void test_LadeSkabsStateLocked_false_id()
-            {
-                _chargeControl.isConnected().Returns(true);
-
-                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs(){id = 123});
-                _rfidReader.RfidDetectedEvent += Raise.EventWith(new RfidDetectedEventArgs(){id = 124});
-
-                _display.Received(1).display("Skabet er låst og din telefon lades. Brug dit RFID tag til at låse op.",1);
-                _display.Received(1).display("Forkert RFID tag",1);
-                
-            }
-            
-            
         }
     }
 }
